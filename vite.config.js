cd11f324de23cc3e5ab42b263d168bb2a1613b67
Sambit Sahoo <sambitsahoo191@gmail.com>
2025-07-26 15:06:31 +0530

    fixed

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	plugins: [react()],
 	server: {
-		port: 3000,
-		proxy: {
-			"/api": {
-				target: "https://chat-be-2h56.onrender.com",
-			},
-		},
-	},
+  proxy: {
+    '/api': {
+      target: 'https://chat-be-2h56.onrender.com',
+      changeOrigin: true,
+      secure: false,  // <--- disables SSL cert verification
+    }
+  }
+}
 });
